0f48421333e7a45b298ef474aa07c5e70bcf6008
Renato Perez <renatoperezc@gmail.com>
2018-02-11 09:37:35 -0600

    setting componentWillUnmount to withErrorHandler to prevent memory leakage

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,16 +8,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     };
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         err => {
           this.setState({ error: err });
         }
       );
+    }
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
     errorConfirmedHandler = () => {
       this.setState({ error: null });
